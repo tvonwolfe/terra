c5bbc216f58d6330d0966e43d056817ed423d4a8
Tony Von Wolfe <vonwolfee@gmail.com>
2020-05-14 22:11:31 -0600

    initial commit

---
+++ src/main/java/com/tonyvonwolfe/terra/TerraServerManagementForMinecraftApplication.java
@@ -0,0 +1,13 @@
+package com.tonyvonwolfe.terra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraServerManagementForMinecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraServerManagementForMinecraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonyvonwolfe/terra/TerraServerManagementForMinecraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyvonwolfe.terra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraServerManagementForMinecraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
