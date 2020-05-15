d97f025d8db279d39358dea071c420914972a071
Tony Von Wolfe <vonwolfee@gmail.com>
2020-05-14 22:30:52 -0600

    update application name to not be so wordy

---
+++ src/test/java/com/tonyvonwolfe/terra/TerraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyvonwolfe.terra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
